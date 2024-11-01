38d48ddd395cbdc23684dcef4061a2f6649be824
R-RafiAhmed <rafiahamed7706@gmail.com>
2024-11-01 16:52:26 +0600

    added price chart

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 // import Dnavbar from "./component/Dnavbar/Dnavbar";
 import Navbar  from "./component/Navbar/Navbar";
 import PriceOptions from "./component/PriceOptions/PriceOptions";
+import LineChart from "./component/LineChart/LineChart";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Navbar></Navbar>
         {/* <Dnavbar></Dnavbar> */}
         <PriceOptions></PriceOptions>
+        <LineChart></LineChart>
       </main>
     </>
   );
---
+++ src/component/LineChart/LineChart.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+// import { LineChart as LChart, Line } from 'recharts';
+import { IoBarChart } from "react-icons/io5";
+import { BarChart, Bar, XAxis, YAxis } from 'recharts';
+
+
+const LineChart = () => {
+
+    const [chart, setChart] = useState([]);
+    useEffect(() => {
+        fetch('chartdata.json')
+        .then(res => res.json())
+        .then(data => setChart(data));
+    },[])
+
+    return (
+        <div className="my-28">
+            <h2 className="text-6xl font-bold text-white text-center mb-20">Price Chart</h2>
+            <div className="flex gap-14">
+                <div>
+                    <BarChart width={1000} height={500} data={chart}>
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <Bar dataKey="basic" fill="lightBlue"></Bar>
+                        <Bar dataKey="standard" fill="yellow"></Bar>
+                        <Bar dataKey="premium" fill="orange"></Bar>
+                        <Bar dataKey="vip" fill="red"></Bar>
+                    </BarChart>
+                    {/* <LChart width={800} height={400} data={chart}>
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <Line dataKey="basic" stroke="lightBlue"></Line>
+                        <Line dataKey="standard" stroke="yellow"></Line>
+                        <Line dataKey="premium" stroke="orange"></Line>
+                        <Line dataKey="vip" stroke="red"></Line>
+                    </LChart> */}
+                </div>
+                <div>
+                    <ul className="text-2xl">
+                        <li className="flex items-center gap-1 text-blue-200"><IoBarChart></IoBarChart>Basic</li>
+                        <li className="flex items-center gap-1 text-yellow-300"><IoBarChart></IoBarChart>Standard</li>
+                        <li className="flex items-center gap-1 text-orange-400"><IoBarChart></IoBarChart>Premium</li>
+                        <li className="flex items-center gap-1 text-red-600"><IoBarChart></IoBarChart>VIP</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LineChart;
